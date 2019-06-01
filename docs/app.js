ff1414a5d236f05693bb92b9844a54923892ba7e
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-06-01 17:44:50 +0200

    fix serviceworker

--- docs/app.js
+++ docs/app.js
@@ -1,65 +1,11 @@
 (function () {
     'use strict';
-
-    // Check to make sure service workers are supported in the current browser,
-    // and that the current page is accessed from a secure origin. Using a
-    // service worker from an insecure origin will trigger JS console errors. See
-    // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-    var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-        // [::1] is the IPv6 localhost address.
-        window.location.hostname === '[::1]' ||
-        // 127.0.0.1/8 is considered localhost for IPv4.
-        window.location.hostname.match(
-            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-        )
-    );
-
-    if ('serviceWorker' in navigator &&
-        (window.location.protocol === 'https:' || isLocalhost)) {
-        console.log('00');
-        navigator.serviceWorker.register('/simonpwa/sw.js')
-            .then(function (registration) {
-                // updatefound is fired if service-worker.js changes.
-                console.log('1');
-                registration.onupdatefound = function () {
-                    console.log('2');
-                    // updatefound is also fired the very first time the SW is installed,
-                    // and there's no need to prompt for a reload at that point.
-                    // So check here to see if the page is already controlled,
-                    // i.e. whether there's an existing service worker.
-                    if (navigator.serviceWorker.controller) {
-                        console.log('3');
-                        // The updatefound event implies that registration.installing is set:
-                        // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-                        var installingWorker = registration.installing;
-
-                        installingWorker.onstatechange = function () {
-                            console.log('4');
-                            switch (installingWorker.state) {
-                                case 'installed':
-                                    console.log('case installed');
-                                    // At this point, the old content will have been purged and the
-                                    // fresh content will have been added to the cache.
-                                    // It's the perfect time to display a "New content is
-                                    // available; please refresh." message in the page's interface.
-                                    break;
-
-                                case 'redundant':
-                                    console.log('case redundant');
-                                    throw new Error('The installing ' +
-                                        'service worker became redundant.');
-
-                                default:
-                                    console.log('default');
-                                // Ignore
-                            }
-                        };
-                    }
-                };
-            }).catch(function (e) {
-                console.error('Error during service worker registration:', e);
-            });
+    
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js')
+            .catch(console.error);
     }
+    
 
     var loader = document.getElementById('loader');
     setTimeout(function () {
@@ -72,11 +18,6 @@
         loader.remove();
     }, 3400);
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js')
-            .catch(console.error);
-    }
-    
     // setup levels
     var levels = [
         [0, 1, 3, 2],
