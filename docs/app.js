c2a1bb295da46cd02502c6e47af296a3e7ecfe0d
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-06-01 17:03:17 +0200

    changed path

--- docs/app.js
+++ docs/app.js
@@ -52,190 +52,190 @@
                 console.error('Error during service worker registration:', e);
             });
     }
-    
-// setup levels
-var levels = [
-    [0, 1, 3, 2],
-    [0, 0, 1, 3, 1],
-    [0, 1, 3, 2, 3, 1, 3],
-    [1, 3, 3, 1, 4, 2],
-    [0, 3, 0, 1, 2, 1, 0]
-];
-var levelIdx = 0;
-var selectedLevel = [];
 
-setLevel(0);
-
-// setup buttons
-var selectedIdx = -1;
-var tmr = null;
-var items = document.querySelectorAll('.btnClick');
-console.log('items', items.length);
-
-//  wire up buttons
-var watchInput = false;
-items.forEach(function (item) {
-    item.addEventListener('click', function () {
-        if (watchInput) {
-            flashItem(item);
-            var clickedItem = parseInt(item.id.split('-')[1], 10);
-            console.log(clickedItem);
-            var expectedItem = selectedLevel[selectedIdx];
-            // check input
-            if (clickedItem === expectedItem) {
-                console.log('CORRECT');
-            } else {
-                console.log('INCORRECT pressed ' + clickedItem + ' expected ' + expectedItem);
-                document.getElementById('incorrect').style.display = 'block';
-            }
-            selectedIdx++;
-            if (selectedIdx === selectedLevel.length) {
-                watchInput = false;
-                setLevel(levelIdx + 1);
-                document.getElementById('correct').style.display = 'block';
-            }
-        }
-    });
-});
-
-document.getElementById('startGame').addEventListener('click', function () {
-    console.log('click start');
-    start();
-});
-
-function flashItem(item) {
-    addClass(item, 'flash');
+    var loader = document.getElementById('loader');
     setTimeout(function () {
-        removeClass(item, 'flash');
-    }, 150);
-}
+        loader.firstChild.classList.add('rotating');
+    }, 700);
+    setTimeout(function () {
+        loader.classList.add('is-hidden');
+    }, 1400);
+    setTimeout(function () {
+        loader.remove();
+    }, 3400);
 
-function setLevel(level) {
-    levelIdx = level;
-    selectedLevel = levels[levelIdx];
-    document.getElementById('level').innerHTML = levelIdx;
-}
-
-function start() {
-    cancelNextItem();
-    selectedIdx = -1;
-    setVisibility('correct', false);
-    setVisibility('incorrect', false);
-    nextItem();
-}
-
-function setVisibility(id, visible) {
-    document.getElementById(id).style.display = visible ? 'block' : 'none';
-    //console.log(id);
-}
-
-function addClass(elem, cls) {
-    if (typeof elem === 'string') {
-        //console.log('addClass elem === string', elem);
-        elem = document.getElementById(elem);
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js').then(function () {
+            console.log("Service Worker Registered");
+        });
     }
-    var i;
-    var found = false;
-    //console.log('addClass elem', elem);
-    var classes = elem.className.split(' ');
-    //console.log(classes);
-    for (i = 0; i < classes.length; i++) {
-        if (classes[i] === cls) {
-            found = true;
-        }
-    }
-    if (!found) {
-        elem.className += ' ' + cls;
-        //console.log(elem.className);
-    }
-}
+    
+    // setup levels
+    var levels = [
+        [0, 1, 3, 2],
+        [0, 0, 1, 3, 1],
+        [0, 1, 3, 2, 3, 1, 3],
+        [1, 3, 3, 1, 4, 2],
+        [0, 3, 0, 1, 2, 1, 0]
+    ];
+    var levelIdx = 0;
+    var selectedLevel = [];
 
-function removeClass(elem, remove) {
-    if (typeof elem === 'string') {
-        elem = document.getElementById(elem);
-    }
-    var newClassName = '';
-    var i;
-    var classes = elem.className.split(' ');
-    for (i = 0; i < classes.length; i++) {
-        if (classes[i] !== remove) {
-            newClassName += classes[i] + ' ';
-        }
-    }
-    elem.className = newClassName;
-}
+    setLevel(0);
 
-// button functions
-function cancelNextItem() {
-    if (tmr) {
-        window.cancelTimeout(tmr);
-    }
-}
+    // setup buttons
+    var selectedIdx = -1;
+    var tmr = null;
+    var items = document.querySelectorAll('.btnClick');
+    console.log('items', items.length);
 
-function clearItems() {
+    //  wire up buttons
+    var watchInput = false;
     items.forEach(function (item) {
-        removeClass(item, 'flash');
+        item.addEventListener('click', function () {
+            if (watchInput) {
+                flashItem(item);
+                var clickedItem = parseInt(item.id.split('-')[1], 10);
+                console.log(clickedItem);
+                var expectedItem = selectedLevel[selectedIdx];
+                // check input
+                if (clickedItem === expectedItem) {
+                    console.log('CORRECT');
+                } else {
+                    console.log('INCORRECT pressed ' + clickedItem + ' expected ' + expectedItem);
+                    document.getElementById('incorrect').style.display = 'block';
+                }
+                selectedIdx++;
+                if (selectedIdx === selectedLevel.length) {
+                    watchInput = false;
+                    setLevel(levelIdx + 1);
+                    document.getElementById('correct').style.display = 'block';
+                }
+            }
+        });
     });
-}
 
-function setItem(selectedIdx) {
-    addClass(items[selectedIdx], 'flash');
-}
+    document.getElementById('startGame').addEventListener('click', function () {
+        console.log('click start');
+        start();
+    });
 
-function nextItem() {
-    tmr = null;
-    var currentIdx = -1;
-    selectedIdx++;
-    if (selectedIdx < selectedLevel.length) {
-        // not finished pattern yet
-        currentIdx = selectedLevel[selectedIdx];
-    }
-    // clear current
-    if (selectedIdx > 0) {
-        clearItems();
-    }
-    // set next
-
-    if (selectedIdx > 0 && currentIdx > -1) {
-        // was previous and next
-        // allow a pause before next flash
-        window.setTimeout(function () {
-            postNext(currentIdx);
+    function flashItem(item) {
+        addClass(item, 'flash');
+        setTimeout(function () {
+            removeClass(item, 'flash');
         }, 150);
-    } else {
-        // no previous flash so just set next
-        postNext(currentIdx);
     }
-}
 
-function postNext(currentIdx) {
-    if (currentIdx > -1) {
-        setItem(currentIdx);
-        tmr = window.setTimeout(nextItem, 800);
-    } else {
-        startWatching();
+    function setLevel(level) {
+        levelIdx = level;
+        selectedLevel = levels[levelIdx];
+        document.getElementById('level').innerHTML = levelIdx;
     }
-}
 
-function startWatching() {
-    selectedIdx = 0;
-    watchInput = true;
-}
+    function start() {
+        cancelNextItem();
+        selectedIdx = -1;
+        setVisibility('correct', false);
+        setVisibility('incorrect', false);
+        nextItem();
+    }
 
-var loader = document.getElementById('loader');
-setTimeout(function () {
-    loader.firstChild.classList.add('rotating');
-}, 700);
-setTimeout(function () {
-    loader.classList.add('is-hidden');
-}, 1400);
-setTimeout(function () {
-    loader.remove();
-}, 3400);
+    function setVisibility(id, visible) {
+        document.getElementById(id).style.display = visible ? 'block' : 'none';
+        //console.log(id);
+    }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function () {
-        console.log("Service Worker Registered");
-    });
-}
+    function addClass(elem, cls) {
+        if (typeof elem === 'string') {
+            //console.log('addClass elem === string', elem);
+            elem = document.getElementById(elem);
+        }
+        var i;
+        var found = false;
+        //console.log('addClass elem', elem);
+        var classes = elem.className.split(' ');
+        //console.log(classes);
+        for (i = 0; i < classes.length; i++) {
+            if (classes[i] === cls) {
+                found = true;
+            }
+        }
+        if (!found) {
+            elem.className += ' ' + cls;
+            //console.log(elem.className);
+        }
+    }
+
+    function removeClass(elem, remove) {
+        if (typeof elem === 'string') {
+            elem = document.getElementById(elem);
+        }
+        var newClassName = '';
+        var i;
+        var classes = elem.className.split(' ');
+        for (i = 0; i < classes.length; i++) {
+            if (classes[i] !== remove) {
+                newClassName += classes[i] + ' ';
+            }
+        }
+        elem.className = newClassName;
+    }
+
+    // button functions
+    function cancelNextItem() {
+        if (tmr) {
+            window.cancelTimeout(tmr);
+        }
+    }
+
+    function clearItems() {
+        items.forEach(function (item) {
+            removeClass(item, 'flash');
+        });
+    }
+
+    function setItem(selectedIdx) {
+        addClass(items[selectedIdx], 'flash');
+    }
+
+    function nextItem() {
+        tmr = null;
+        var currentIdx = -1;
+        selectedIdx++;
+        if (selectedIdx < selectedLevel.length) {
+            // not finished pattern yet
+            currentIdx = selectedLevel[selectedIdx];
+        }
+        // clear current
+        if (selectedIdx > 0) {
+            clearItems();
+        }
+        // set next
+
+        if (selectedIdx > 0 && currentIdx > -1) {
+            // was previous and next
+            // allow a pause before next flash
+            window.setTimeout(function () {
+                postNext(currentIdx);
+            }, 150);
+        } else {
+            // no previous flash so just set next
+            postNext(currentIdx);
+        }
+    }
+
+    function postNext(currentIdx) {
+        if (currentIdx > -1) {
+            setItem(currentIdx);
+            tmr = window.setTimeout(nextItem, 800);
+        } else {
+            startWatching();
+        }
+    }
+
+    function startWatching() {
+        selectedIdx = 0;
+        watchInput = true;
+    }
 })();
