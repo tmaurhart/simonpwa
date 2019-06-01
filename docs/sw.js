bf703367024b073b20a8bbd5540a6b453eb3f12c
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-06-01 17:32:50 +0200

    fix serviceworker

--- docs/sw.js
+++ docs/sw.js
@@ -4,6 +4,39 @@ self.addEventListener('install', (event) => {
         caches.open(swCache)
             .then(cache => cache.addAll([
                 '/simonpwa/',
+                'index.html',                
+                'app.js',
+                'css/styles.css',
+                'manifest.webmanifest',
+                'icons/icon512.png',
+                'icons/icon16.png',
+                'icons/android-chrome-192x192.png',
+                'icons/android-chrome-512x512.png',
+                'icons/apple-touch-icon.png',
+                'icons/favicon-16x16.png',
+                'icons/favicon-32x32.png',
+                'icons/favicon.ico',
+                'icons/mstile-150x150.png',
+                'icons/safari-pinned-tab.svg'
+            ]))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open(swCache)
+            .then(cache => cache.match(event.request))
+    );
+});
+
+
+/*
+const swCache = 'simon-pwa-v1';
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(swCache)
+            .then(cache => cache.addAll([
+                '/simonpwa/',
                 'index.html',
                 'css/styles.css',
                 'app.js',
@@ -45,3 +78,4 @@ self.addEventListener('fetch', function (e) {
         })
     );
 });
+*/
