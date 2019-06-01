31ef106736165c3698deac5406688d3d4f95b101
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-06-01 17:20:20 +0200

    fix serviceworker

--- docs/app.js
+++ docs/app.js
@@ -19,19 +19,24 @@
         navigator.serviceWorker.register('/sw.js')
             .then(function (registration) {
                 // updatefound is fired if service-worker.js changes.
+                console.log('1');
                 registration.onupdatefound = function () {
+                    console.log('2');
                     // updatefound is also fired the very first time the SW is installed,
                     // and there's no need to prompt for a reload at that point.
                     // So check here to see if the page is already controlled,
                     // i.e. whether there's an existing service worker.
                     if (navigator.serviceWorker.controller) {
+                        console.log('3');
                         // The updatefound event implies that registration.installing is set:
                         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
                         var installingWorker = registration.installing;
 
                         installingWorker.onstatechange = function () {
+                            console.log('4');
                             switch (installingWorker.state) {
                                 case 'installed':
+                                    console.log('case installed');
                                     // At this point, the old content will have been purged and the
                                     // fresh content will have been added to the cache.
                                     // It's the perfect time to display a "New content is
@@ -39,10 +44,12 @@
                                     break;
 
                                 case 'redundant':
+                                    console.log('case redundant');
                                     throw new Error('The installing ' +
                                         'service worker became redundant.');
 
                                 default:
+                                    console.log('default');
                                 // Ignore
                             }
                         };
