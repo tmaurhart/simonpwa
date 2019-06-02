28af9e0d0fe67b91a02a9426657d5c10b22d5a74
Thomas Maurhart <thomas.maurhart@gmail.com>
2019-06-02 09:14:30 +0200

    add some noise

--- docs/app.js
+++ docs/app.js
@@ -8,7 +8,7 @@
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('/sw.js')
+            .register('sw.js')
             .then(function () { console.log("Service Worker Registered"); });
     }
     
